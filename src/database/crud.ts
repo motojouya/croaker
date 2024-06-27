76f8adc799488d3c158280adb3eb22b1fb71bceb
Takuya Sugiyama (@motojouya) <motojouya@gmail.com>
2024-06-27 04:44:47 +0000

    working

--- src/database/crud.ts
+++ src/database/crud.ts
@@ -2,7 +2,11 @@ import {
   Kysely,
   Insertable,
   Selectable,
-  Updateable
+  Updateable,
+  InsertObject,
+  UpdateObject,
+  AnyColumn,
+  ExtractTypeFromReferenceExpression,
 } from 'kysely'
 import { Database } from '@/database/type';
 
@@ -10,8 +14,10 @@ import { Database } from '@/database/type';
 // ALEのtscだと限界あるのかな。とりあえずほっておいて、実際にコンパイルしてみて結果を見る。
 // 型としては、見る限りあってそうで、大丈夫そうならignoreしてしまう
 
+type KeyValue<T extends keyof Database & string> = [AnyColumn<Database, T>, ExtractTypeFromReferenceExpression<Database, T, AnyColumn<Database, T>>];
+
 export function create(db: Kysely<Database>) {
-  return async function <T extends keyof Database & string>(tableName: T, newRecord: Insertable<Database[T]>): Promise<Database[T][]> {
+  return async function <T extends keyof Database & string>(tableName: T, newRecord: InsertObject<Database, T>): Promise<Database[T]> {
     return await db.insertInto(tableName)
       .values(newRecord)
       .returningAll()
@@ -23,9 +29,10 @@ export function read(db: Kysely<Database>) {
   return async function <T extends keyof Database & string>(tableName: T, criteria: Partial<Selectable<Database[T]>>): Promise<Database[T][]> {
     let query = db.selectFrom(tableName);
 
-    Object.entries(criteria).forEach(([key, value]) => {
+    // @ts-ignore
+    Object.entries(criteria).forEach(([key, value]: KeyValue<T>) => {
       if (Object.hasOwn(criteria, key)) {
-        command = command.where(key, '=', value);
+        query = query.where(key, '=', value);
       }
     });
 
@@ -34,6 +41,20 @@ export function read(db: Kysely<Database>) {
 }
 
 export function update(db: Kysely<Database>) {
+  return async function <T extends keyof Database & string>(tableName: T, criteria: Partial<Selectable<Database[T]>>, updateWith: UpdateObject<Database, T>): Promise<Database[T][]> {
+    let command = db.updateTable(tableName).set(updateWith);
+
+    Object.entries(criteria).forEach(([key, value]) => {
+      if (Object.hasOwn(criteria, key)) {
+        command = command.where(key, '=', value);
+      }
+    });
+
+    return await command.returningAll().execute();
+  };
+}
+
+export function updateUseAbles(db: Kysely<Database>) {
   return async function <T extends keyof Database & string>(tableName: T, criteria: Partial<Selectable<Database[T]>>, updateWith: Updateable<Database[T]>): Promise<Database[T][]> {
     let command = db.updateTable(tableName).set(updateWith);
 
