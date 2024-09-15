1a23f657abbbeed2350ba786c08c0c4daea22f3c
Takuya Sugiyama (@motojouya) <motojouya@gmail.com>
2024-09-15 12:55:58 +0000

    e

--- migrate.sh
+++ migrate.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+set -Ceux
+
 MIGRATE_COMMAND=$1
-DATABASE_PATH=$2
 DATABASE_FILE=$3
 DB_BACKET_NAME=$4
 DB_BACKET_PATH=$5
@@ -11,15 +12,10 @@ if [ "$MIGRATE_COMMAND" != "latest" ] && [ "$MIGRATE_COMMAND" != "rollback" ]; t
   exit 1;
 fi
 
-litestream restore -o "$DATABASE_PATH/$DATABASE_FILE" "gcs://$DB_BACKET_NAME/$DB_BACKET_PATH/$DATABASE_FILE" -if-db-not-exists -if-replica-exists
-RESTORE_PID=$!
-echo "$RESTORE_PID"
-
-litestream replicate  "$DATABASE_PATH/$DATABASE_FILE" "gcs://$DB_BACKET_NAME/$DB_BACKET_PATH/$DATABASE_FILE" &
+litestream restore -if-db-not-exists -if-replica-exists -o "$DATABASE_FILE" "gcs://$DB_BACKET_NAME/$DB_BACKET_PATH/$DATABASE_FILE"
+litestream replicate "$DATABASE_FILE" "gcs://$DB_BACKET_NAME/$DB_BACKET_PATH/$DATABASE_FILE" &
 REPLICATE_PID=$!
-echo "$REPLICATE_PID"
 
 npx kysely migrate:$MIGRATE_COMMAND
-echo "migrated!"
 sleep 10
 kill $REPLICATE_PID
